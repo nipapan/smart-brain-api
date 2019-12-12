e78eb019a4bb63cbb5599e57835b6dc545542df0
Nipapan Aom <nipapan@maqe.com>
2019-12-12 13:35:55 +0700

    Done code analysis

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR_API_KEY_HERE'
+ apiKey: '637664e5b9c54b769a81b55dd311e603'
 });
 
 const handleApiCall = (req, res) => {
